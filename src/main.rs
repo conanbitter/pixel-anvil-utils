b802764e4ba0b2f83c54b87f9d63f797aeb437d2
Conan Bitter <conan.bitter@gmail.com>
2025-12-19 20:26:18 +0300

    Try getargs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
 
+use getargs::Opt;
 use image::{ImageReader, RgbaImage};
 
 use crate::{
     color::Color16,
-    converters::{convert_ordered4, convert_posterize},
+    converters::{convert_fs, convert_ordered4, convert_ordered8, convert_posterize},
     plane::Plane,
 };
 
@@ -12,11 +13,9 @@ mod color;
 mod converters;
 mod plane;
 
-fn image_convert<P: AsRef<Path>>(
-    input: P,
-    output: P,
-    dithering: fn(&RgbaImage) -> Plane<Color16>,
-) -> anyhow::Result<()> {
+type DitheringMethod = fn(&RgbaImage) -> Plane<Color16>;
+
+fn image_convert<P: AsRef<Path>>(input: P, output: P, dithering: DitheringMethod) -> anyhow::Result<()> {
     let img = ImageReader::open(input)?.decode()?.to_rgba8();
     let res_img = dithering(&img);
     res_img.save(output)?;
@@ -24,8 +23,54 @@ fn image_convert<P: AsRef<Path>>(
     Ok(())
 }
 
+fn choose_dithering(param: &str) -> DitheringMethod {
+    match param {
+        "none" => convert_posterize,
+        "fs" => convert_fs,
+        "ord4" => convert_ordered4,
+        "ord8" => convert_ordered8,
+        _ => convert_posterize,
+    }
+}
+
+fn command_img<'arg, I: Iterator<Item = &'arg str>>(opts: &mut getargs::Options<&'arg str, I>) -> anyhow::Result<()> {
+    let mut dithering: DitheringMethod = convert_posterize;
+    let mut output = Path::new("");
+
+    while let Some(opt) = opts.next_opt().unwrap() {
+        match opt {
+            Opt::Short('d') | Opt::Long("dither") => {
+                dithering = choose_dithering(opts.value().unwrap());
+            }
+            Opt::Short('o') | Opt::Long("output") => {
+                output = Path::new(opts.value().unwrap());
+            }
+            _ => {}
+        }
+    }
+
+    println!("dith: {:?}, out: {:?}", dithering, output);
+
+    for pos in opts.positionals() {
+        println!("positional arg: {pos}");
+    }
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
-    image_convert("assets/rainbow.png", "assets/rainbow.img", convert_ordered4)?;
+    let args = wild::args().skip(1).collect::<Vec<_>>();
+    let mut opts = getargs::Options::new(args.iter().map(String::as_str));
+
+    let command = opts.next_positional();
+
+    match command {
+        None => println!("No command"),
+        Some("img") => command_img(&mut opts)?,
+        Some(_) => println!("Unknown command"),
+    }
+
+    //image_convert("assets/rainbow.png", "assets/rainbow.img", convert_ordered4)?;
 
     Ok(())
 }
